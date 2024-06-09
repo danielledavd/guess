189d657cd8833a2107aeddf53480ffdc9048120f
danielledavd <danielledavid422@gmail.com>
2024-06-10 01:04:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 var attempts = 10;
 
-function checkGuess(){
+function checkGuess() {
   attempts--;
   const inputElement = document.getElementById("guess");
-  let guess = inputElement.value;
+  let guess = parseInt(inputElement.value); // Make sure the input is treated as a number
   const feedbackElement = document.getElementById("feedback");
-  while(attempts > 0){
-    if (guess===randomNumber){
-      feedbackElement.innerHTML = "Success!";
-      feedbackElement.style.color = "Green";
-      attempts = 0;
-      break;
-    }
-    else if(guess<randomNumber){
-      feedbackElement.innerHTML = "Too low! Try again" + "<br>" + "Attempts left: " + attempts;
-      break;
-    }
-    else{
-      feedbackElement.innerHTML = "Too high! Try again";
-      print("Attempts left: " + attempts);
-      break;
-    }
+
+  if (guess === randomNumber) {
+    feedbackElement.innerHTML = "Success!";
+    feedbackElement.style.color = "Green";
+  } else if (guess < randomNumber) {
+    feedbackElement.innerHTML = "Too low! Try again. Attempts left: " + attempts;
+  } else {
+    feedbackElement.innerHTML = "Too high! Try again. Attempts left: " + attempts;
   }
-  if(attempts === 0 && guess != randomNumber){
-    feedbackElement.innerHTML = "Game over"
+
+  if (attempts === 0 && guess !== randomNumber) {
+    feedbackElement.innerHTML = "Game over. The correct number was " + randomNumber;
     feedbackElement.style.color = "Red";
   }
 }
+
